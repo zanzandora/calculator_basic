2948e810b441efbae1c9b284270988c0cd827bc0
theLazyBoy_mmTu <maiminhtu130803@gmail.com>
2024-09-25 10:38:01 +0700

    add calculate func, condition operator to calculate, logic to calculate

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const keys = document.querySelector(".calculator__keys");
 const display = document.querySelector(".calculator__display");
+const calculator = document.querySelector('.calculator');
 
 keys.addEventListener("click", (e) => {
   // todo Kiểm tra user click đúng vào các tag button
@@ -11,9 +12,7 @@ keys.addEventListener("click", (e) => {
     if (action === "clear") {
       console.log("clear key");
     }
-    if (action === "calculate") {
-      console.log("calculate key");
-    }
+    
 
     // !chúng ta cần chú ý 2 điều:
     // ? số đã được click
@@ -45,7 +44,68 @@ keys.addEventListener("click", (e) => {
       action === "divide"
     ) {
       key.classList.add("is-keydown");
+      //todo chúng ta muốn cập nhật màn hình hiển thị thành phím đã nhấp. Trước khi thực hiện, chúng ta cần một cách để biết phím trước đó có phải là phím toán tử hay không.
+      calculator.dataset.previousKeyType = 'operator';
+      // *lưu giá trị đầu( giá trị trước khi cleared) và toán tử đã chọn
+      calculator.dataset.firstValue = displayNum
+      calculator.dataset.operator = action
+    }
+    const previousKeyType = calculator.dataset.previousKeyType
+    if (!action) {
+      if (displayNum === '0' || previousKeyType === 'operator') {
+        display.textContent = digit
+      } else {
+        display.textContent =  + digit
+      }
+      
+    }
+    // todo thực hiện phép tính khi click enter
+    if (action === 'calculate') {
+      // *lấy giá trị thứ hai ( giá trị hiện tại )
+      let secondValue = displayNum
+      const operator = calculator.dataset.operator
+      let firstValue = calculator.dataset.firstValue
+
+  display.textContent = calculate(firstValue, operator, secondValue)
+
     }
   }
+
 });
+
+
+function calculate(firstValue, operator, secondValue) {
+  let result = '';
+  switch (operator) { 
+    case 'add':
+      result = parseFloat(firstValue) + parseFloat(secondValue);
+      break;
+      
+    case'subtract':
+      result = parseFloat(firstValue) - parseFloat(secondValue);
+      break;
+    case 'multiply':
+      result = parseFloat(firstValue) * parseFloat(secondValue);
+      break;
+    case 'divide':
+      if (parseFloat(secondValue) === 0) {
+        result = 'Error'
+      } else {
+        result = parseFloat(firstValue) / parseFloat(secondValue);
+      }
+      break;
+      
+    // case 'exponentiation':
+  
+    default:
+      result = parseFloat(secondValue);
+      
+  }
+  // todo ta cần làm tròn các số có thập phân gần bằng 0 
+  if (Math.abs(result - Math.round(result)) < 0.01) {
+    // *Làm tròn nếu phần thập phân gần bằng 0
+    return Math.round(result);
+  }
+  return parseFloat(result.toFixed(8));
+}
 
